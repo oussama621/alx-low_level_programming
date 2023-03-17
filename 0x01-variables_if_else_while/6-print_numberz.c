98c0cf1651918d54858f7a1f21f650d8134d5cf9
oussama621 <oussama.elmaaloum1@gmail.com>
2023-03-17 08:56:29 -0700

    numbers task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - main function that prints numbers from 0 to 9
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i = '0');
+	putchar('\n');
+	return (0);
+}
