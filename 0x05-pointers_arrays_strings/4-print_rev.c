4392ba3c350b1921af5be94247ffd455d616513a
oussama621 <oussama.elmaaloum1@gmail.com>
2023-03-27 08:50:48 -0700

    prints in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints in reverse
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int j;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (j = longi; j > 0; j--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
+
